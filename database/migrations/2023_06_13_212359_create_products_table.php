ab3cd74c8cf444090b1b2a3b5a4810e675962113
faouzia akdi <fawziaakdi2510@gmail.com>
2023-06-15 20:04:39 +0100

    add migrations

---
+++ database/migrations/2023_06_13_212359_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string("name");
+            $table->mediumText("description");
+            $table->string("size")->nullable(true);
+            $table->double("price")->nullable(true);
+            $table->double("quantity")->nullable(true);
+            $table->text("type")->nullable(true);
+
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
